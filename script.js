fe8f893a39552ea901fb8ff878a1aad0ab89643d
denis-ttk-1975 <denis.ttk@gmail.com>
2021-06-09 12:11:39 +0300

    TaskFulfilled!

--- script.js
+++ script.js
@@ -23,6 +23,24 @@
 
 'use strict';
 
-let numberOfFilms
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', 100);
 
+alert(numberOfFilms);
+
+let personalMovieDB = {count: NaN,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+console.log(personalMovieDB);
+
+for (let i=0; i<=1; i++)  {
+
+	let filmName = prompt('Один из последних просмотренных фильмов?');
+	let filmScore = prompt('На сколько оцените его?');
+	personalMovieDB.movies[filmName] = filmScore;
+}
+
+console.log(personalMovieDB);
 
